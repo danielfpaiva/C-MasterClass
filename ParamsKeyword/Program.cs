791ab3acb13434c5035f1fb039dd51d4226940b2
Daniel Paiva <danielfonsecapaiva@gmail.com>
2021-09-27 15:37:53 +0100

    Params Keyword
    This allow us to create methods/functions that can receive as many arguments as the user wants

---
+++ ParamsKeyword/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+namespace ParamsKeyword
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //ParamsMethod("This", "is", "just", "a", "silly", "lesson");
+
+            //ParamsMethod2(50,3.14f,'@',"The Hobbit");
+
+            ParamsMin(5,8,3,8,9,5,-2);
+        }
+
+        public static int ParamsMin(params int[] numbers)
+        {
+            int min = int.MaxValue;
+
+            foreach(int number in numbers)
+            {
+                min = Math.Min(number, min);
+                Console.WriteLine("Now min is {0}", min);
+            }
+
+            return min;
+        }
+
+        public static void ParamsMethod(params string[] sentence)
+        {
+            for (int i = 0; i< sentence.Length; i++)
+            {
+                Console.WriteLine(sentence[i] + "  ");
+            }
+        }
+
+        public static void ParamsMethod2(params object[] stuff)
+        {
+            foreach (object obj in stuff)
+            {
+                Console.WriteLine(obj + "  ");
+            }
+            Console.WriteLine();
+        }
+    }
+}

0df35b329ff59c901bdef8f4c8cf8493fab96b2b
Daniel Paiva <daniel.paiva@lsi.pt>
2022-10-06 18:26:45 +0100

    Interface Demo Chapter

---
+++ Interface/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace InterfaceDemo
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Ticket t1 = new Ticket(10);
+            Ticket t2 = new Ticket(10);
+
+            Console.WriteLine(t2.Equals(t1));
+        }
+    }
+}
---
+++ Interface/Ticket.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InterfaceDemo
+{
+    class Ticket : IEquatable<Ticket>
+    {
+        public int DurationInHours;
+
+        public Ticket(int durationInHours)
+        {
+            DurationInHours = durationInHours;
+        }
+
+        public bool Equals (Ticket other)
+        {
+            return DurationInHours == other.DurationInHours;
+        }
+
+    }
+}
